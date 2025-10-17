2dead75d713a5f284abad90bcd385677a4769577
sergiogamezpacheco-debug <sergiogamezpacheco@gmail.com>
2025-10-17 18:02:47 +0200

    uu

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,9 +1,6 @@
 // js/firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-auth.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-analytics.js";
 
-// Tu configuración de Firebase (la que tú me diste)
 const firebaseConfig = {
   apiKey: "AIzaSyCZNZjQctvsDwQiwYd-89aLcEQiSfd2E6Y",
   authDomain: "oporail.firebaseapp.com",
@@ -14,7 +11,4 @@ const firebaseConfig = {
   measurementId: "G-Y75GHN32PJ"
 };
 
-// Inicializar Firebase
 export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const analytics = getAnalytics(app);
